306e3ff099bbe2165917f6d4bc2fe7b5d4e8d94b
KhoalaS <s4khleee@uni-trier.de>
2023-11-10 01:01:30 +0100

    add tests for rrules.go

---
+++ pkg/hypergraph/rrules_test.go
@@ -0,0 +1,37 @@
+package hypergraph
+
+import "testing"
+
+func TestApproxVertexDominationRule(t *testing.T){
+	vSize := 8
+	eSize := 2
+
+	vertices := make([]Vertex, vSize)
+	edges := make([]Edge, eSize)
+
+	for i := 0; i < vSize; i++ {
+		vertices[i] = NewVertex(i, 0)
+	}
+
+	edges[0] = NewEdge(0,1,2)
+	edges[1] = NewEdge(0,2,7)
+
+	g := NewHyperGraph(vertices, edges)
+	c := make(map[int]bool)
+
+	g1, c1 := ApproxVertexDominationRule(g, c)
+
+	// expected outcomes for c1:
+	// {2,7}, {1,2}, {0,7}, {0,1}
+	if (c1[2] && c1[7]) || (c1[1] && c1[2]) || (c1[0] && c1[7]) || (c1[0] && c1[1]) {
+		t.Log(c1)
+		if len(c1) != 2 {
+			t.Fatalf("Partial solution is wrong.")
+		}
+	}
+
+	if len(g1.Edges) != 0 {
+		t.Fatalf("Number of edges are incorrect, there are %d edges in the current graph.",len(g1.Edges))
+	}
+
+}
