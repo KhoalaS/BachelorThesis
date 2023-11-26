13eb133638297e65d5e94271c9ed88413fe27e17
KhoalaS <s4khleee@uni-trier.de>
2023-11-26 19:07:43 +0100

    fix getSubsetsRec using the whole data array as subset

--- pkg/hypergraph/algorithms.go
+++ pkg/hypergraph/algorithms.go
@@ -3,29 +3,29 @@ package hypergraph
 import (
 	"container/list"
 )
-
-func getSubsetsRec(arr *[]int32, i int, end int, s int, data *[]int32, index int, subsets *list.List){
+func getSubsetsRec(arr *[]int32, i int, n int, s int, data *[]int32, index int, subsets *list.List){
     if index ==  s{
         subset := make([]int32, s)    
-        for j, val := range *data {
-            subset[j] = val
+        for j:= 0; j < index; j++ {
+            subset[j] = (*data)[j]
         }
         subsets.PushBack(subset)
         return
     }
 
-    if i >= end {
+    if i >= n {
         return
     }
 
     (*data)[index] = (*arr)[i]
     
-    getSubsetsRec(arr, i+1, end, s, data, index+1, subsets)
-    getSubsetsRec(arr, i+1, end, s, data, index, subsets)
+    getSubsetsRec(arr, i+1, n, s, data, index+1, subsets)
+    getSubsetsRec(arr, i+1, n, s, data, index, subsets)
 }
 
-func twoSum(arr []IdValueHolder, n int) ([][]int32) { 
-    N := int32(n)
+// Time Complexity: n
+func twoSum(arr []IdValueHolder, t int) ([][]int32) { 
+    N := int32(t)
     lookup := make(map[int32][]IdValueHolder)
     solutions := [][]int32{}
 
