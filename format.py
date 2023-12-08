257e27202ebdf9a9e3f4c82d94eea056af125170
KhoalaS <s4khleee@uni-trier.de>
2023-12-08 18:43:07 +0100

    add script to auto-generate a PDF from README.MD with pandoc.

---
+++ format.py
@@ -0,0 +1,18 @@
+import re
+import os
+
+f = open("README.MD")
+text = "# This document was auto-generated using pandoc.\n\n"
+for line in f:
+    text += line
+
+r = r"(\$\$)\s(\\begin\{align\*\}\s[^$]*)(\$\$)"
+
+new = re.sub(r, r"\2", text)
+
+out = open("./out/out.MD", "w+")
+out.write(new)
+out.close()
+
+code = os.system("pandoc -V geometry:margin=1in out/out.MD -o notes.pdf")
+exit(code)
