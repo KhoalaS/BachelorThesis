53438cdd306ed0ff1a2b7a05c457677e8070068b
KhoalaS <s4khleee@uni-trier.de>
2023-12-22 17:37:26 +0100

    add flags for more control over graph generation in charts and standalone

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
 	"os"
 	"strings"
 
@@ -15,63 +14,82 @@ import (
 	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
-func makeChart() {
+func makeChart(u int, evr int, maxv int, checkpoint int) {
 	var baseSize int32 = 10
 	baseSizes := []int32{}
 	var g *hypergraph.HyperGraph
 	var maxVert int32 = 10000
+	if maxv > 0 {
+		maxVert = int32(maxv)
+	}
+	maxratio := 20
+	var maxest float64 = 3
 
-	labels := make([]int, 20)
+	if evr > 0 {
+		maxratio = evr
+	}
+
+	labels := make([]int, maxratio)
 	lineSeries := make(map[int32][]opts.LineData)
-	barLabels := []string{"kTiny", "kEdgeDom", "kVertDom", "kTri", "kApVertDom", "kSmall", "kApDoubleVertDom"}
-	barLabelsShort := []string{"Tiny", "EDom", "VDom", "Tri", "ApVDom", "Small", "ApDVDom"}
+	barLabels := []string{"kTiny", "kEdgeDom", "kVertDom", "kTri", "kApVertDom", "kSmall", "kApDoubleVertDom", "kFallback"}
+	barLabelsShort := []string{"Tiny", "EDom", "VDom", "Tri", "ApVDom", "Small", "ApDVDom", "F3"}
 
 	barSeries1 := make(map[int32][]opts.BarData)
-	barSeries10 := make(map[int32][]opts.BarData)
+	barSeries2 := make(map[int32][]opts.BarData)
 
 	for baseSize <= maxVert {
 		baseSizes = append(baseSizes, baseSize)
 		lineSeries[baseSize] = []opts.LineData{}
-		for i := 1; i <= 20; i++ {
+		for i := 1; i <= maxratio; i++ {
 			labels[i-1] = i
-			g = hypergraph.GenerateTestGraph(baseSize, int32(i)*baseSize, true)
+			if u > 0 {
+				g = hypergraph.GenerateUniformTestGraph(baseSize, int32(i)*baseSize, u)
+			} else {
+				g = hypergraph.GenerateTestGraph(baseSize, int32(i)*baseSize, true)
+			}
 			c := make(map[int32]bool)
+			execs := make(map[string]int)
 
-			execs, _ := alg.ApplyRules(g, c, 1000000)
-
+			alg.ThreeHS_2ApprPoly(g, c, int(baseSize)*i, execs)
 			var nom float64 = 0
 			var denom float64 = 0
 
 			for key, val := range execs {
-				if key == "kEdgeDom" || key == "kVertDom"{
+				if key == "kEdgeDom" || key == "kVertDom" {
 					continue
 				}
 				nom += float64(alg.Ratios[key].A * val)
 				denom += float64(alg.Ratios[key].B * val)
 			}
+
 			//fmt.Println("Nom: " ,nom)
 			//fmt.Println("Denom: " ,denom)
+			maxest = nom / denom
 
+			lineSeries[baseSize] = append(lineSeries[baseSize], opts.LineData{Value: fmt.Sprintf("%.2f", (nom / denom))})
 
-
-			lineSeries[baseSize] = append(lineSeries[baseSize], opts.LineData{Value: fmt.Sprintf("%.2f",(nom / denom))})
-
-			if _, ex := barSeries10[baseSize]; !ex {
-				barSeries10[baseSize] = []opts.BarData{}
+			if _, ex := barSeries2[baseSize]; !ex {
+				barSeries2[baseSize] = []opts.BarData{}
 				barSeries1[baseSize] = []opts.BarData{}
 			}
-			if i == 10 {
+			if i == checkpoint {
 				for _, v := range barLabels {
-					barSeries10[baseSize] = append(barSeries10[baseSize], opts.BarData{Value: execs[v]})
+					barSeries2[baseSize] = append(barSeries2[baseSize], opts.BarData{Value: execs[v]})
 				}
 			} else if i == 1 {
 				for _, v := range barLabels {
 					barSeries1[baseSize] = append(barSeries1[baseSize], opts.BarData{Value: execs[v]})
 				}
 			}
-			fmt.Println("Edges/Vertices Factor:", i, "|", "Approximation Factor:", nom/denom)
+			fmt.Println(len(g.Edges), "Edges/Vertices Factor:", i, "|", "Approximation Factor:", nom/denom)
 		}
 		baseSize = baseSize * 10
+	}
+
+	if maxest <= 2 {
+		maxest = 2.0
+	} else {
+		maxest = 3.0
 	}
 
 	page := components.NewPage()
@@ -88,15 +106,13 @@ func makeChart() {
 				},
 			}},
 		),
-		charts.WithYAxisOpts(opts.YAxis{Min: 1, Max: 2, Name: "est. Approximation Factor"}),
+		charts.WithYAxisOpts(opts.YAxis{Min: 1, Max: maxest, Name: "est. Approximation Factor"}),
 		charts.WithXAxisOpts(opts.XAxis{Name: "#Edges\\#Vertices"}),
 		charts.WithTooltipOpts(opts.Tooltip{Show: true}),
 		charts.WithLegendOpts(opts.Legend{Show: true, Right: "80px"}),
 		charts.WithTooltipOpts(opts.Tooltip{Show: true, Trigger: "axis"}),
 	)
 	line.SetXAxis(labels)
-
-
 
 	for _, val := range baseSizes {
 		l := fmt.Sprintf("%dK Vertices", val/1000)
@@ -115,7 +131,7 @@ func makeChart() {
 
 	bar1.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{
-			Title: "#Rule Executions",
+			Title:    "#Rule Executions",
 			Subtitle: "[#Edges/#Vertices = 1]",
 		}),
 		charts.WithToolboxOpts(opts.Toolbox{
@@ -133,8 +149,8 @@ func makeChart() {
 
 	bar10.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{
-			Title: "#Rule Executions",
-			Subtitle: "[#Edges/#Vertices = 10]",
+			Title:    "#Rule Executions",
+			Subtitle: fmt.Sprintf("[#Edges/#Vertices = %d]", checkpoint),
 		}),
 		charts.WithToolboxOpts(opts.Toolbox{
 			Show:  true,
@@ -162,10 +178,10 @@ func makeChart() {
 		)
 
 	bar10.SetXAxis(barLabelsShort).
-		AddSeries("10 Vertices", barSeries10[10]).
-		AddSeries("100 Vertices", barSeries10[100]).
-		AddSeries("1K Vertices", barSeries10[1000]).
-		AddSeries("10K Vertices", barSeries10[10000]).
+		AddSeries("10 Vertices", barSeries2[10]).
+		AddSeries("100 Vertices", barSeries2[100]).
+		AddSeries("1K Vertices", barSeries2[1000]).
+		AddSeries("10K Vertices", barSeries2[10000]).
 		SetSeriesOptions(
 			charts.WithLabelOpts(opts.Label{
 				Show:     true,
@@ -178,31 +194,87 @@ func makeChart() {
 	page.Render(f)
 }
 
+func getRatio(execs map[string]int) float64 {
+	var nom float64 = 0
+	var denom float64 = 0
+
+	for key, val := range execs {
+		nom += float64(alg.Ratios[key].A * val)
+		denom += float64(alg.Ratios[key].B * val)
+	}
+	return nom / denom
+}
+
 func main() {
-	input := flag.String("f", "", "Filepath to graphml file.")
+	input := flag.String("i", "", "Filepath to graphml input file.")
 	n := flag.Int("n", 10000, "Number of vertices if no graph file supplied.")
-	m := flag.Int("m", int(rand.Int31n(10000)) * (1 + int(rand.Int31n(20))), "Number of edges if no graph file supplied.")
+	m := flag.Int("m", 20000, "Number of edges if no graph file supplied.")
 	K := flag.Int("k", 0, "The parameter k.")
+	chart := flag.Bool("c", false, "Make charts.")
+	u := flag.Int("u", 0, "Generate a u-uniform graph.")
+	evr := flag.Int("evr", 0, "Maximum ratio |E|\\|V| to compute for random graphs.")
+	maxv := flag.Int("maxv", 0, "Maximum vertices for random graphs used in charts.")
+	preset := flag.String("p", "", "Use a preconfigured chart preset. For available presets run with 'list -p'.")
+	list := flag.NewFlagSet("list", flag.ExitOnError)
+	printPreset := list.Bool("p", false, "")
+
 	flag.Parse()
+	
+	if os.Args[1] == "list" {
+		list.Parse(os.Args[2:])
+		if *printPreset {
+			fmt.Println("u3\t 3-uniform graphs, E\\V ratio of 5, 1K maximum vertices")
+			fmt.Println("u2\t 2-uniform graphs, E\\V ratio 10, 10K maximum vertices")
+			return
+		}
+	}
+
+	if len(strings.Trim(*preset, " ")) > 0 {
+		checkpoint := 10
+		switch strings.Trim(*preset, " ") {
+		case "u3":
+			*u = 3
+			*evr = 5
+			*maxv = 1000
+			checkpoint = 5
+		case "u2":
+			*u = 2
+			*evr = 10
+			*maxv = 10000
+		}
+		makeChart(*u, *evr, *maxv, checkpoint)
+		return
+	}
+
+	if *chart {
+		makeChart(*u, *evr, *maxv, 10)
+		return
+	}
 
 	if *K == 0 {
-		*K = int(float64(0.4)*float64(*m))
+		*K = int(float64(0.4) * float64(*m))
 	}
-	
+
 	var g *hypergraph.HyperGraph
 	if len(strings.Trim(*input, " ")) > 0 {
 		g = hypergraph.ReadFromFile(strings.Trim(*input, " "))
-	}else{
-		fmt.Printf("Using random graph with \n\t%d vertices\n\t%d edges\n\tk=%d\n", *n, *m, *K)
-		g = hypergraph.GenerateTestGraph(int32(*n), int32(*m), true)
+	} else {
+		fmt.Printf("Using random graph with: \n\t%d vertices\n\t%d edges\n\tk=%d\n", *n, *m, *K)
+		if *u > 0 {
+			g = hypergraph.GenerateUniformTestGraph(int32(*n), int32(*m), *u)
+		} else {
+			g = hypergraph.GenerateTestGraph(int32(*n), int32(*m), true)
+		}
 	}
 
 	c := make(map[int32]bool)
-
-	ex, hs := alg.ThreeHS_2ApprGeneral(g, c, *K)
-	if ex {
-		fmt.Printf("Found a 3-Hitting-Set of size %d <= 2K = %d", len(hs), 2*(*K))
-	}else{
-		fmt.Printf("Did not find a 3-Hitting-Set of size <= 2K = %d", 2*(*K))
+	execs := make(map[string]int)
+	fmt.Println("Start Algorithm")
+	ex, hs, execs := alg.ThreeHS_2ApprGeneral(g, c, *K, execs)
+	if ex || (len(hs) < 2*(*K) && len(g.Edges) == 0) {
+		fmt.Printf("Found a 3-Hitting-Set of size %d <= 2K = %d\n", len(hs), 2*(*K))
+		fmt.Printf("Estimated Approximation Factor: %.2f\n", getRatio(execs))
+	} else {
+		fmt.Printf("Did not find a 3-Hitting-Set of size <= 2K = %d\n", 2*(*K))
 	}
 }
