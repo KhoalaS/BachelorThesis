115b8a2ffec2d58970be5250220146ebc8c96de7
KhoalaS <s4khleee@uni-trier.de>
2024-01-03 22:17:12 +0100

    add colors based on community to graph drawing

--- pkg/hypergraph/draw.py
+++ pkg/hypergraph/draw.py
@@ -2,19 +2,62 @@ import matplotlib.pyplot as plt
 import hypernetx as hnx
 import sys
 
-file = open(sys.argv[1], "r") 
-
 edges = {}
+data = {}
+
+file = open(sys.argv[1], "r")
+
+datafile = None
+if len(sys.argv) > 2:
+    datafile = open(sys.argv[2], "r")
+    for line in datafile:
+        s = line.strip().split()
+        data.update({s[0]: {"com": s[1]}})
+    
+
 
 i = 0
 for line in file:
     s = line.strip().split()
-    edges.update({i : tuple(s)})
+    edges.update({i: tuple(s)})
     i += 1
 
-g = hnx.Hypergraph(edges)
+g = hnx.Hypergraph(edges, node_properties=data)
 
-plt.subplots(figsize=(5,5))
-hnx.draw(g, with_edge_labels=False, with_edge_counts=False, with_node_labels=False, with_node_counts=False, node_radius=0.5)
+colors_map = {
+    "0": "red",
+    "1": "green",
+    "2": "blue",
+    "3": "yellow",
+    "4": "pink",
+    "5": "orange",
+    "6": "purple",
+    "7": "brown",
+    "8": "gray",
+    "9": "olive",
+    "10":"cyan",
+}
+
+plt.subplots(figsize=(5, 5))
+
+if datafile == None:
+    hnx.draw(g, with_edge_labels=False,
+             with_edge_counts=False,
+             with_node_labels=True,
+             with_node_counts=False,
+             node_radius=0.5)
+else:
+    colors = [
+        colors_map[g.get_properties(id=v, level=1, prop_name="com")]
+        for v in list(g.nodes)
+    ]
+    hnx.draw(g, with_edge_labels=False,
+             with_edge_counts=False,
+             with_node_labels=True,
+             with_node_counts=False,
+             node_radius=0.5,
+             nodes_kwargs={
+                 'facecolors': colors
+             })
 
 plt.show()
