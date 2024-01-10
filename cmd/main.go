b5e4fcc8e051430c2f6cc5bf2d92185dcf9fef9f
KhoalaS <s4khleee@uni-trier.de>
2024-01-10 17:20:36 +0100

    change default F3 function to linear

--- cmd/main.go
+++ cmd/main.go
@@ -71,7 +71,7 @@ func makeChart(pa float64, u int, evr int, maxv int, checkpoint int, fixRatio st
 			c := make(map[int32]bool)
 			execs := make(map[string]int)
 
-			alg.ThreeHS_F3ApprPoly(g, c, execs, 0, alg.Exp)
+			alg.ThreeHS_F3ApprPoly(g, c, execs, 0, alg.Linear)
 			var nom float64 = 0
 			var denom float64 = 0
 
@@ -343,7 +343,7 @@ func main() {
 		k -= kFront
 	}
 
-	ex, hs, execs := alg.ThreeHS_F3ApprPoly(g, c, execs, prio, alg.Exp)
+	ex, hs, execs := alg.ThreeHS_F3ApprPoly(g, c, execs, prio, alg.Linear)
 
 	pprof.StopCPUProfile()
 	if ex {
