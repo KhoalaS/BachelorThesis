3ee6051decdbab63e28f9f88032a5e4f023e072c
KhoalaS <s4khleee@uni-trier.de>
2024-01-12 20:52:25 +0100

    add binomial coefficient function

--- pkg/hypergraph/utils.go
+++ pkg/hypergraph/utils.go
@@ -2,6 +2,7 @@ package hypergraph
 
 import (
 	"log"
+	"math"
 	"sort"
 	"strconv"
 	"time"
@@ -19,9 +20,8 @@ func getHash(arr []int32) string {
 	}
 
 	return in
-	
-}
 
+}
 
 func SetMinus(e Edge, elem int32) ([]int32, bool) {
 	arr := []int32{}
@@ -39,8 +39,23 @@ func SetMinus(e Edge, elem int32) ([]int32, bool) {
 	return arr, lenBefore != lenAfter
 }
 
-
-func LogTime(start time.Time, name string){
+func LogTime(start time.Time, name string) {
 	stop := time.Since(start)
 	log.Printf("%s took %s\n", name, stop)
+}
+
+func binomialCoefficient(n int, k int) int {
+	//wenn 2*k > n dann k = n-k
+	//ergebnis = 1
+	//für i = 1 bis k
+	//    ergebnis = ergebnis * (n + 1 - i) / i
+	//rückgabe ergebnis
+	if 2*k > n {
+		k = n - k
+	}
+	c := 1.0
+	for i := 1; i <= k; i++ {
+		c = c * float64(n + 1 - i) / float64(i)
+	}
+	return int(math.Ceil(c))
 }
--- pkg/hypergraph/utils_test.go
+++ pkg/hypergraph/utils_test.go
@@ -1,1 +1,15 @@
 package hypergraph
+
+import (
+	"log"
+	"testing"
+)
+
+func TestBinomialCoefficient(t *testing.T) {
+	c := binomialCoefficient(1000, 3)
+	exp := 166167000
+
+	if c != exp {
+		log.Fatalf("Wrong solution, got %d, expected %d.", c, exp)
+	}
+}
