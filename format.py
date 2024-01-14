0c4f52dc2efc49f003319f379af14d9c8380a8d4
KhoalaS <s4khleee@uni-trier.de>
2024-01-15 00:13:25 +0100

    split notes from README

--- format.py
+++ format.py
@@ -1,37 +1,24 @@
 import re
 import os
 
-f = open("README.MD")
-text = "# This document was auto-generated using pandoc.\n\n"
-
-header = (
-    "---",
-    "title: Implementation and evaluation of a self-monitoring approximation algorithm for 3-Hitting-Set",
-    "header-includes: |",
-    "\t\\usepackage{algorithm2e}",
-    "...")
-
-latex_commands = ["\RestyleAlgo{ruled}", "\SetAlgoLined",
-                  "\DontPrintSemicolon"]
-
-text = text + "\n".join(header) + "\n"
-text = text + "\n".join(latex_commands) + "\n"
-
+f = open("notes.MD")
+text = ""
 
 for line in f:
     text += line
 
 r_0 = r"(\$\$)\s(\\begin\{align\*\}\s[^$]*)(\$\$)"
-r_1 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}\[H\].*)(\$\$)", re.DOTALL)
+r_1 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}\[H\].*?end{algorithm})\s(\$\$)", re.DOTALL)
+r_2 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}.*?end{algorithm})\s(\$\$)", re.DOTALL)
 
 out = open("./out/out.MD", "w+")
-new = re.sub(r_0, r"\2", text)
-new = re.sub(r_1, r"\2", new)
-out.write(new)
+text = re.sub(r_0, r"\2", text)
+text = re.sub(r_1, r"\2", text)
+text = re.sub(r_2, r"\2", text)
 
-
+out.write(text)
 out.close()
 
 code = os.system(
-    "pandoc -V geometry:margin=1in out/out.MD -o notes.pdf --bibliography=./lit.bib --csl=./ieee.csl")
+    "pandoc --number-sections --toc -V geometry:margin=1in out/out.MD -o notes.pdf --bibliography=./lit.bib --csl=./ieee.csl")
 exit(code)
