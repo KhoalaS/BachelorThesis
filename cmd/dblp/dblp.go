2f3f1179afedf4b9183a2527e14e6588a478a66d
KhoalaS <s4khleee@uni-trier.de>
2024-01-20 16:30:53 +0100

    add debug printing

--- cmd/dblp/dblp.go
+++ cmd/dblp/dblp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"encoding/xml"
 	"errors"
@@ -48,16 +49,22 @@ func main(){
 		return enc.NewDecoder().Reader(reader), nil
 	}
 	var entities map[string]string
-	entitiyFile, _ := os.Open("../entities.json")
+	entitiyFile, err := os.Open("../entities.json")
+	if err != nil{
+		log.Fatal("Could not open entities.json")
+	}
 	entitiyBytes, _ := io.ReadAll(entitiyFile)
 	json.Unmarshal(entitiyBytes, &entities)
+	entitiyFile.Close()
 
 	var authorIdCounter int32 =0
 	authorId := make(map[string]int32)
 	coauthor := make(map[int32]map[int32]bool)
 
+	pubtypes := make(map[string]int)
+
 	decoder.Entity = entities
-	r := regexp.MustCompile("article|inproceedings|proceedings|book|incollection|phdthesis|mastersthesis|www")
+	r := regexp.MustCompile("article|inproceedings|proceedings|book|incollection|phdthesis|mastersthesis")
 	count := 0
 
 	for {
@@ -76,6 +83,7 @@ func main(){
 				continue
 			}
 			if r.MatchString(tt.Name.Local) {
+				pubtypes[tt.Name.Local]++
 				var p Publication
 				err := decoder.DecodeElement(&p, &tt)
 				if err != nil {
@@ -100,24 +108,43 @@ func main(){
 			}
 		}
 	}
+	f.Close()
 
-	fmt.Printf("\nProcessed all publications\n")
+	fmt.Println()
+	pubSum := 0
+	for _, val := range pubtypes {
+		pubSum += val
+	}
+	fmt.Printf("Processed %d publications\n", pubSum)
+	fmt.Printf("Found %d authors\n", len(authorId))
+
+	for t, v := range pubtypes {
+		fmt.Println(t, v)
+	}
+
 	fmt.Println("Writing coauthor graph to file")
 
 	out, err := os.Create("./coauthor.txt")
 	if err != nil {
 		log.Fatal("Could not open file coauthor.txt")
 	}
+	defer out.Close()
 
 	count = 0
+	bw := bufio.NewWriterSize(out, 8192)
 	
 	for k, val := range coauthor {
 		for v := range val {
 			line := strconv.Itoa(int(k)) + " " + strconv.Itoa(int(v)) + "\n"
-			out.WriteString(line)
-			fmt.Printf("Processed %d edges\r", count)
-			count++
+			bw.Write([]byte(line))
 		}
+		fmt.Printf("Processed %d authors\r", count)
+		count++
 	}
+	err = bw.Flush()
+	if err != nil {
+		log.Fatal("Could not flush buffer for coauthor.txt")
+	}
+	fmt.Println()
 	fmt.Println("Processed all edges")
 }
