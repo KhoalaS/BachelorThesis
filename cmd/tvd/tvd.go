f0b5d368865755b5eae0392604dd5885dfdc0e52
KhoalaS <s4khleee@uni-trier.de>
2024-01-20 18:19:03 +0100

    rework tvd

--- cmd/tvd/tvd.go
+++ cmd/tvd/tvd.go
@@ -3,92 +3,87 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/KhoalaS/BachelorThesis/pkg/alg"
 	"github.com/KhoalaS/BachelorThesis/pkg/hypergraph"
 )
 
-func ReduceToHS(t []map[int32]bool) *hypergraph.HyperGraph{
-	g := hypergraph.NewHyperGraph()
-
-	hashes := make(map[string]bool)
-	arr := make([]int32, 3)
-
-	for _, triangle := range t {
-		i := 0
-		for v := range triangle {
-			arr[i] = v
-			i++
+func flagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
 		}
-		hash := hypergraph.GetHash(arr)
-		if hashes[hash] {
-			continue
-		}else{
-			for v := range triangle {
-				g.AddVertex(v, 0)
-			}
-			g.AddEdge(arr...)
-			hashes[hash] = true
-		}
-	}
-	return g
+	})
+	return found
 }
 
-func main(){
+func main() {
 	in := flag.String("i", "", "path to input graph file")
 	n := flag.Int("n", 1000, "number of vertices")
 	p := flag.Float64("p", 0.5, "probability of adding an edge")
 	evr := flag.Float64("evr", 0.0, "targetted edge/vertex ratio, takes priority over p")
-	logging := flag.Int("log", 0, "log the number of rule executions, do log many runs")
+	logging := flag.Int("log", 1, "log the number of rule executions, do log many runs")
 
 	flag.Parse()
 
-	var g *hypergraph.HyperGraph
+	adjList := make(map[int32]map[int32]bool)
 
 	graphtype := "CUSTOM"
-	if len(*in) > 0 {
-		g = hypergraph.ReadFromFile(*in)
-	}else{
-		g = hypergraph.UniformERGraph(*n, *p, *evr, 2)
-		graphtype = "ER"
-	}
+	var g *hypergraph.HyperGraph
 
-	t := hypergraph.TriangleDetection(g)
+	timestamp := time.Now().Unix()
+	masterfilename := fmt.Sprintf("master_%s_%d.csv", graphtype, timestamp)
 
-	g = &hypergraph.HyperGraph{}
-	g = ReduceToHS(t)
+	for i := 0; i < *logging; i++ {
+		adjList = make(map[int32]map[int32]bool)
+		var a int
+		var b int
 
-	fmt.Printf("Found %d triangles\n",len(g.Edges))
-	c := make(map[int32]bool)
-
-	if *logging > 0 {
-		l_evr := float64(*evr)
-		if *evr == 0 {
-			l_evr = float64(len(g.Edges)) / float64(len(g.Vertices))
+		if len(*in) > 0 {
+			hypergraph.ReadFromFileSimpleCallback(*in, func(line string) {
+				spl := strings.Split(line, " ")
+				a, _ = strconv.Atoi(spl[0])
+				b, _ = strconv.Atoi(spl[1])
+				if _, ex := adjList[int32(a)]; !ex {
+					adjList[int32(a)] = make(map[int32]bool)
+				}
+				if _, ex := adjList[int32(b)]; !ex {
+					adjList[int32(b)] = make(map[int32]bool)
+				}
+				adjList[int32(a)][int32(b)] = true
+				adjList[int32(b)][int32(a)] = true
+			})
+		} else {
+			hypergraph.UniformERGraphCallback(*n, *p, *evr, 2, func(edge []int32) {
+				if _, ex := adjList[edge[0]]; !ex {
+					adjList[edge[0]] = make(map[int32]bool)
+				}
+				if _, ex := adjList[edge[1]]; !ex {
+					adjList[edge[1]] = make(map[int32]bool)
+				}
+				adjList[edge[0]][edge[1]] = true
+				adjList[edge[1]][edge[0]] = true
+			})
+			graphtype = "ER"
 		}
-		timestamp := time.Now().Unix()
-		masterfilename := fmt.Sprintf("master_%s_%.2f_%d.csv", graphtype, l_evr, timestamp)
-		for i := 0; i < *logging; i++ {
-			alg.LoggingThreeHS_F3ApprPoly(g, c, graphtype, masterfilename, i)
-			if i == *logging - 1 {
-				break
-			}
 
-			if len(*in) > 0 {
-				g = hypergraph.ReadFromFile(*in)
-			}else{
-				g = hypergraph.UniformERGraph(*n, *p, *evr, 2)
-			}
+		fmt.Println("Start Triangle detection and problem reduction")
+		g = hypergraph.TriangleDetection(adjList)
+		c := make(map[int32]bool)
 
-			t = hypergraph.TriangleDetection(g)
-			g = ReduceToHS(t)
-			c = make(map[int32]bool)
+		var execs map[string]int
+
+		fmt.Println("Start 3-HS algorithm")
+		if flagPassed("log") {
+			execs = alg.LoggingThreeHS_F3ApprPoly(g, c, graphtype, masterfilename, i)
+		} else {
+			execs = alg.ThreeHS_F3ApprPoly(g, c, 0)
 		}
-	}else{
-		execs := alg.ThreeHS_F3ApprPoly(g, c, 0)
 		fmt.Println(execs)
 		fmt.Println("Est. Approximation Factor:", alg.GetRatio(execs))
 	}
 }
-
