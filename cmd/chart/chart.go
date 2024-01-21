4791aa67e717bde980f26ddc53416e8d9897431a
KhoalaS <s4khleee@uni-trier.de>
2024-01-21 22:29:07 +0100

    add charting code

--- cmd/chart/chart.go
+++ cmd/chart/chart.go
@@ -1,5 +1,111 @@
 package main
 
-func main(){
-	
+import (
+	"encoding/csv"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func main() {
+	in := flag.String("d", "./data", "path to folder with csv files")
+	start := flag.Int("start", 1, "if placeholder is used in path, first value to be used")
+	end := flag.Int("end", 1, "if placeholder is used in path, last value to be used")
+
+	flag.Parse()
+
+	ratios := [][]float64{}
+	ratioItems := []opts.BoxPlotData{}
+
+	box := charts.NewBoxPlot()
+	box.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{Title: "est. approximation factor"}),
+		charts.WithYAxisOpts(opts.YAxis{Min: 1.8}),
+		charts.WithTooltipOpts(opts.Tooltip{Show: true}),
+		charts.WithToolboxOpts(opts.Toolbox{
+			Show:  true,
+			Right: "20%",
+			Feature: &opts.ToolBoxFeature{
+				SaveAsImage: &opts.ToolBoxFeatureSaveAsImage{
+					Show:  true,
+					Type:  "png",
+					Title: "Save",
+				},
+			},
+		}),
+	)
+
+	for i := *start; i <= *end; i++ {
+		var data []fs.DirEntry
+		var err error
+
+		inrepl := strings.ReplaceAll(*in, "%", strconv.Itoa(i))
+		data, err = os.ReadDir(inrepl)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		reg := regexp.MustCompile("master")
+		for j := 0; j < len(data); j++ {
+			if !reg.MatchString(data[j].Name()) {
+				continue
+			}
+
+			//spl := strings.Split(data[i].Name(), "_")
+			//evr, _ := strconv.ParseInt(spl[2],0,0)
+
+			file, err := os.Open(inrepl + "/" + data[j].Name())
+			if err != nil {
+				log.Fatalf("Could not open file %s", inrepl+"/"+data[j].Name())
+			}
+
+			reader := csv.NewReader(file)
+			reader.Comma = ';'
+
+			reader.Read()
+			ratios = append(ratios, []float64{})
+
+			for {
+				record, err := reader.Read()
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				ratio, _ := strconv.ParseFloat(record[0], 64)
+				ratios[i-*start] = append(ratios[i-*start], ratio)
+			}
+
+			ratioItems = append(ratioItems, opts.BoxPlotData{Value: ratios[i-*start]})
+
+		}
+
+	}
+	fmt.Println(ratios)
+
+	labels := []int{}
+	for i:=*start; i<=*end; i++ {
+		labels = append(labels, i)
+	}
+
+	box.SetXAxis(labels)
+	box.AddSeries("Ratio", ratioItems)
+	outfile, err := os.OpenFile("./out/out.html", os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal("Could not open ./out/out.html")
+	}
+	box.Render(outfile)
 }
