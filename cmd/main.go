87280c1824c538918143d33b120640b3c1e19873
KhoalaS <s4khleee@uni-trier.de>
2024-01-22 17:09:23 +0100

    remove go-echarts dep, move to pyecharts

--- cmd/main.go
+++ cmd/main.go
@@ -11,209 +11,10 @@ import (
 
 	"github.com/KhoalaS/BachelorThesis/pkg/alg"
 	"github.com/KhoalaS/BachelorThesis/pkg/hypergraph"
-
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/components"
-	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
-func makeChart(pa float64, u int, evr int, maxv int, checkpoint int, fixRatio string) {
-	var baseSize int32 = 10
-	baseSizes := []int32{}
-	var g *hypergraph.HyperGraph
-	var maxest float64 = 2
-
-	var maxVert int32 = 10000
-	if maxv > 0 {
-		maxVert = int32(maxv)
-	}
-
-	maxratio := 20
-	if evr > 0 {
-		maxratio = evr
-	}
-
-	if pa > 0 {
-		maxratio = 1
-	}
-
-	if checkpoint > maxratio {
-		checkpoint = maxratio
-	}
-
-	labels := make([]int, maxratio)
-	lineSeries := make(map[int32][]opts.LineData)
-	barLabels := []string{"kTiny", "kEdgeDom", "kVertDom", "kTri", "kExtTri", "kApVertDom", "kSmall", "kApDoubleVertDom", "kSmallEdgeDegTwo", "kFallback"}
-	barLabelsShort := []string{"Tiny", "EDom", "VDom", "Tri", "ETri", "ApVDom", "Small", "ApDVDom", "SmED2", "F3"}
-
-	barSeries1 := make(map[int32][]opts.BarData)
-	barSeries2 := make(map[int32][]opts.BarData)
-
-	for baseSize <= maxVert {
-		baseSizes = append(baseSizes, baseSize)
-		lineSeries[baseSize] = []opts.LineData{}
-		for i := 1; i <= maxratio; i++ {
-			labels[i-1] = i
-			if u > 0 {
-				g = hypergraph.UniformTestGraph(baseSize, int32(i)*baseSize, u)
-			} else if len(fixRatio) > 0 {
-				spl := strings.Split(fixRatio, ",")
-				ratios := make([]int, len(spl))
-				for i, val := range spl {
-					valInt, _ := strconv.Atoi(val)
-					ratios[i] = valInt
-				}
-				g = hypergraph.FixDistTestGraph(baseSize, int32(i)*baseSize, ratios)
-			} else if pa > 0 {
-				g = hypergraph.PrefAttachmentGraph(baseSize, pa, 3)
-			} else {
-				g = hypergraph.TestGraph(baseSize, int32(i)*baseSize, true)
-			}
-			c := make(map[int32]bool)
-
-			execs := alg.ThreeHS_F3ApprPoly(g, c, 0)
-			var nom float64 = 0
-			var denom float64 = 0
-
-			for key, val := range execs {
-				if key == "kEdgeDom" || key == "kVertDom" {
-					continue
-				}
-				nom += float64(alg.Ratios[key].A * val)
-				denom += float64(alg.Ratios[key].B * val)
-			}
-
-			//fmt.Println("Nom: " ,nom)
-			//fmt.Println("Denom: " ,denom)
-			if nom/denom > maxest {
-				maxest = 3
-			}
-
-			lineSeries[baseSize] = append(lineSeries[baseSize], opts.LineData{Value: fmt.Sprintf("%.2f", (nom / denom))})
-
-			if _, ex := barSeries2[baseSize]; !ex {
-				barSeries2[baseSize] = []opts.BarData{}
-				barSeries1[baseSize] = []opts.BarData{}
-			}
-			if i == checkpoint {
-				for _, v := range barLabels {
-					barSeries2[baseSize] = append(barSeries2[baseSize], opts.BarData{Value: execs[v]})
-				}
-			} else if i == 1 {
-				for _, v := range barLabels {
-					barSeries1[baseSize] = append(barSeries1[baseSize], opts.BarData{Value: execs[v]})
-				}
-			}
-			fmt.Println(len(g.Edges), "Edges/Vertices Factor:", i, "|", "Approximation Factor:", nom/denom)
-		}
-		baseSize = baseSize * 10
-	}
-
-	page := components.NewPage()
-	line := charts.NewLine()
-	line.SetGlobalOptions(
-		charts.WithToolboxOpts(opts.Toolbox{
-			Show:  true,
-			Right: "20%",
-			Feature: &opts.ToolBoxFeature{
-				SaveAsImage: &opts.ToolBoxFeatureSaveAsImage{
-					Show:  true,
-					Type:  "png",
-					Title: "Save",
-				},
-			}},
-		),
-		charts.WithYAxisOpts(opts.YAxis{Min: 1, Max: maxest, Name: "est. Approximation Factor"}),
-		charts.WithXAxisOpts(opts.XAxis{Name: "#Edges\\#Vertices"}),
-		charts.WithTooltipOpts(opts.Tooltip{Show: true}),
-		charts.WithLegendOpts(opts.Legend{Show: true, Right: "80px"}),
-		charts.WithTooltipOpts(opts.Tooltip{Show: true, Trigger: "axis"}),
-	)
-	line.SetXAxis(labels)
-
-	for _, val := range baseSizes {
-		l := fmt.Sprintf("%dK Vertices", val/1000)
-		if val < 1000 {
-			l = fmt.Sprintf("%d Vertices", val)
-		}
-		line.AddSeries(l, lineSeries[val]).SetSeriesOptions(
-			charts.WithLineChartOpts(opts.LineChart{
-				ShowSymbol: true,
-			}),
-		)
-	}
-
-	bar1 := charts.NewBar()
-	bar2 := charts.NewBar()
-
-	bar1.SetGlobalOptions(
-		charts.WithXAxisOpts(opts.XAxis{AxisLabel: &opts.AxisLabel{Rotate: 45, Show: true, ShowMinLabel: true, ShowMaxLabel: true}}),
-		charts.WithTitleOpts(opts.Title{
-			Title:    "#Rule Executions",
-			Subtitle: "[#Edges/#Vertices = 1]",
-		}),
-		charts.WithToolboxOpts(opts.Toolbox{
-			Show:  true,
-			Right: "20%",
-			Feature: &opts.ToolBoxFeature{
-				SaveAsImage: &opts.ToolBoxFeatureSaveAsImage{
-					Show:  true,
-					Type:  "png",
-					Title: "save",
-				},
-			}},
-		),
-	)
-	bar2.SetGlobalOptions(
-		charts.WithXAxisOpts(opts.XAxis{AxisLabel: &opts.AxisLabel{Rotate: 45, Show: true, ShowMinLabel: true, ShowMaxLabel: true}}),
-		charts.WithTitleOpts(opts.Title{
-			Title:    "#Rule Executions",
-			Subtitle: fmt.Sprintf("[#Edges/#Vertices = %d]", checkpoint),
-		}),
-		charts.WithToolboxOpts(opts.Toolbox{
-			Show:  true,
-			Right: "20%",
-			Feature: &opts.ToolBoxFeature{
-				SaveAsImage: &opts.ToolBoxFeatureSaveAsImage{
-					Show:  true,
-					Type:  "png",
-					Title: "save",
-				},
-			}},
-		),
-	)
-
-	bar1.SetXAxis(barLabelsShort).
-		AddSeries("10 Vertices", barSeries1[10]).
-		AddSeries("100 Vertices", barSeries1[100]).
-		AddSeries("1K Vertices", barSeries1[1000]).
-		AddSeries("10K Vertices", barSeries1[10000]).
-		SetSeriesOptions(
-			charts.WithLabelOpts(opts.Label{
-				Show:     true,
-				Position: "top",
-			}),
-		)
-
-	bar2.SetXAxis(barLabelsShort).
-		AddSeries("10 Vertices", barSeries2[10]).
-		AddSeries("100 Vertices", barSeries2[100]).
-		AddSeries("1K Vertices", barSeries2[1000]).
-		AddSeries("10K Vertices", barSeries2[10000]).
-		SetSeriesOptions(
-			charts.WithLabelOpts(opts.Label{
-				Show:     true,
-				Position: "top",
-			}),
-		)
-
-	page.AddCharts(line, bar1, bar2)
-	f, _ := os.Create("approx_factor_chart.html")
-	page.Render(f)
-}
-
 func makeHypergraph(input string, u int, f string, n int, m int, prefAttach float64, prefAttachMod bool, er bool, evr int) (*hypergraph.HyperGraph, string) {
-	g := hypergraph.NewHyperGraph()
+	var g *hypergraph.HyperGraph
 	graphtype := "STD"
 
 	if len(strings.Trim(input, " ")) > 0 {
@@ -250,11 +51,9 @@ func main() {
 	input := flag.String("i", "", "Filepath to input file.")
 	n := flag.Int("n", 10000, "Number of vertices if no graph file supplied.")
 	m := flag.Int("m", 20000, "Number of edges if no graph file supplied.")
-	chart := flag.Bool("c", false, "Make charts.")
 	u := flag.Int("u", 0, "Generate a u-uniform graph.")
 	f := flag.String("f", "", "Generate a random hypergraph with fixed ratios for the edge sizes.")
 	evr := flag.Int("evr", 0, "Maximum ratio |E|/|V| to compute for random graphs.")
-	maxv := flag.Int("maxv", 0, "Maximum vertices for random graphs used in charts.")
 	profile := flag.Bool("prof", false, "Make CPU profile")
 	export := flag.String("o", "", "Export the generated graph with the given string as filename. The will create a 'graphs' folder where the file is located.")
 	exportSimple := flag.String("os", "", "Export the generated graph to the given filepath.")
@@ -264,48 +63,7 @@ func main() {
 	logging := flag.Int("log", 0, "")
 	outdir := flag.String("d", "./data", "")
 
-	preset := flag.String("p", "", "Use a preconfigured chart preset. For available presets run with 'list -p'.")
-	list := flag.NewFlagSet("list", flag.ExitOnError)
-	printPreset := list.Bool("p", false, "")
-
 	flag.Parse()
-
-	if len(os.Args) > 1 && os.Args[1] == "list" {
-		list.Parse(os.Args[2:])
-		if *printPreset {
-			fmt.Println("u3\t 3-uniform graphs, E\\V ratio of 5, 1K maximum vertices")
-			fmt.Println("u2\t 2-uniform graphs, E\\V ratio 10, 10K maximum vertices")
-			fmt.Println("pa05\t preferential attachment graphs, probabilty 0.5 of attaching a new vertex, 10K maximum vertices")
-			return
-		}
-	}
-
-	if len(strings.Trim(*preset, " ")) > 0 {
-		checkpoint := 10
-		switch strings.Trim(*preset, " ") {
-		case "u3":
-			*u = 3
-			*evr = 5
-			*maxv = 1000
-			checkpoint = 5
-			*prefAttach = 0
-		case "u2":
-			*u = 2
-			*evr = 10
-			*maxv = 10000
-			*prefAttach = 0
-		case "pa05":
-			*prefAttach = 0.5
-			checkpoint = -1
-		}
-		makeChart(*prefAttach, *u, *evr, *maxv, checkpoint, *f)
-		return
-	}
-
-	if *chart {
-		makeChart(*prefAttach, *u, *evr, *maxv, 10, *f)
-		return
-	}
 
 	g, graphtype := makeHypergraph(*input, *u, *f, *n, *m, *prefAttach, *prefAttachMod, *er, *evr)
 	c := make(map[int32]bool)
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/KhoalaS/BachelorThesis
 
 go 1.18
 
-require github.com/go-echarts/go-echarts/v2 v2.3.2
-
-require golang.org/x/text v0.14.0 // indirect
+require golang.org/x/text v0.14.0
