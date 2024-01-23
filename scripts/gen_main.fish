d55e847e1b6786bed864f11ba2fec979d14a6b0a
KhoalaS <s4khleee@uni-trier.de>
2024-01-23 23:24:08 +0100

    fix missing option

--- scripts/gen_main.fish
+++ scripts/gen_main.fish
@@ -1,5 +1,5 @@
 #!/bin/fish
 
 for i in (seq 1 20)
-    go run cmd/main.go -n=1000 -evr=$i -log=100 -d=data/eru3_"$i"_1K
+    go run cmd/main.go -er -n=1000 -evr=$i -log=100 -d=data/eru3_"$i"_1K
 end
