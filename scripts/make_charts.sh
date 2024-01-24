b22d85e9c190ba73aa5334e9ad76427b8d4a53fa
KhoalaS <s4khleee@uni-trier.de>
2024-01-24 23:41:14 +0100

    update comment in script

--- scripts/make_charts.sh
+++ scripts/make_charts.sh
@@ -6,5 +6,5 @@ python scripts/chart_single.py data/dblp -e scripts/.env.dblp
 python scripts/chart.py data/pa_%_1K -s 2 -t 5 -m 2 -m 5 -e scripts/pa.env -o out/pa_02_05.html
 # er vtd
 python scripts/chart.py data/er_%_1K -s 10 -t 25 -m 10 -m 20 -e scripts/ervtd.env -o out/er_10_25_vtd.html
-# er vtd
+# eru3
 python scripts/chart.py data/eru3_%_1K -s 1 -t 20 -m 10 -m 20 -e scripts/eru3.env -o out/eru3_1_20.html
