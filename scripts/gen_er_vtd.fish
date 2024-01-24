a290a853b99b14c00309214ce54b4192662a5007
KhoalaS <s4khleee@uni-trier.de>
2024-01-24 21:43:49 +0100

    remove fish scripts

--- scripts/gen_er_vtd.fish
+++
@@ -1,5 +0,0 @@
-#!/bin/fish
-
-for i in (seq 10 25)
-  go run cmd/tvd/tvd.go -n=1000 -evr=$i -log=100 -d=data/er_"$i"_1K
-end
--- scripts/gen_main.fish
+++
@@ -1,5 +0,0 @@
-#!/bin/fish
-
-for i in (seq 1 20)
-    go run cmd/main.go -er -n=1000 -evr=$i -log=100 -d=data/eru3_"$i"_1K
-end
