f684d3f2668520bdaac66bdac0cfd02a497c4e9f
KhoalaS <s4khleee@uni-trier.de>
2024-02-06 17:15:00 +0100

    move frontier functions

--- pkg/hypergraph/utils.go
+++ pkg/hypergraph/utils.go
@@ -60,54 +60,6 @@ func binomialCoefficient(n int, k int) int {
 	return int(math.Ceil(c))
 }
 
-func GetFrontierGraph(g *HyperGraph, level int, remId int32) *HyperGraph {
-	g2 := NewHyperGraph()
-	frontier := make(map[int32]bool)
-	remEdge := g.Edges[remId]
-	hashes := make(map[string]bool)
-
-	for v := range g.Edges[remId].V {
-		for e := range g.IncMap[v] {
-			for w := range g.Edges[e].V {
-				if !g.Edges[remId].V[w] {
-					frontier[w] = true
-					g2.AddVertex(w, 0)
-				}
-			}
-		}
-	}
-
-	for i := 0; i < level; i++ {
-		nextFrontier := make(map[int32]bool)
-		for v := range frontier {
-			for e := range g.IncMap[v] {
-				found := true
-				for w := range g.Edges[e].V {
-					if remEdge.V[w] {
-						found = false
-						break
-					}
-				}
-				if found {
-					hash := g.Edges[e].getHash()
-					if !hashes[hash] {
-						hashes[hash] = true
-						g2.AddEdgeMap(g.Edges[e].V)
-						for w := range g.Edges[e].V {
-							if !frontier[w] {
-								g2.AddVertex(w, 0)
-								nextFrontier[w] = true
-							}
-						}
-					}
-				}
-			}
-		}
-		frontier = nextFrontier
-	}
-
-	return g2
-}
 
 func getRatio(execs map[string]int) float64 {
 	var nom float64 = 0
