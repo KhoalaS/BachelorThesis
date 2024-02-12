d01523fe55cc2c5f0fd8f53ba757d8f91d8e174e
KhoalaS <s4khleee@uni-trier.de>
2024-02-12 17:54:27 +0100

    update Frontier test class with basic logging

--- pkg/hypergraph/frontier_test.go
+++ pkg/hypergraph/frontier_test.go
@@ -1,6 +1,7 @@
 package hypergraph
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -17,7 +18,16 @@ func TestGetFrontierGraph(t *testing.T) {
 	g.AddEdge(3, 4)
 	g.AddEdge(4, 5)
 
-	gf := GetFrontierGraph(g, 2, 0)
-	t.Log(gf)
 
+	// This will start the frontier at the vertices of edges incident to 0 and 1 (0 and 1 excluded).
+	// This results in a initial frontier with the vertex 2.
+	// Since we want to go 2 levels deep, only the edges (2,3) and (3,4) should be in
+	// in the new graph gf.
+	gf := GetFrontierGraph(g, 2, 0)
+	ExpandFrontier(gf, g, 2)
+	t.Log(gf)
+	for _, v := range gf.Vertices {
+		fmt.Println(v.Id, v.Data)
+	}
+	t.Log(gf.VertexFrontier)
 }
