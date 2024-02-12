c718ac0166c9839e6ca5f8f3659e40b27d548a85
KhoalaS <s4khleee@uni-trier.de>
2024-02-12 17:55:01 +0100

    rework Frontier functions

--- pkg/hypergraph/frontier.go
+++ pkg/hypergraph/frontier.go
@@ -6,7 +6,6 @@ func GetFrontierGraph(g *HyperGraph, level int, remId int32) *HyperGraph {
 	g2 := NewHyperGraph()
 	frontier := make(map[int32]bool)
 	remEdge := g.Edges[remId]
-	hashes := make(map[string]bool)
 
 	for v := range remEdge.V {
 		for e := range g.IncMap[v] {
@@ -25,54 +24,112 @@ func GetFrontierGraph(g *HyperGraph, level int, remId int32) *HyperGraph {
 		}
 	}
 
-
 	fmt.Println("Init frontier:", frontier)
 
 	for i := 0; i < level; i++ {
 		nextFrontier := make(map[int32]bool)
 		for v := range frontier {
 			for e := range g.IncMap[v] {
-				found := true
-				for w := range g.Edges[e].V {
-					if remEdge.V[w] {
-						found = false
-						break
-					}
-				}
-				if found {
-					hash := g.Edges[e].getHash()
-					if !hashes[hash] {
-						hashes[hash] = true
-						g2.AddEdgeMapWLayer(g.Edges[e].V, i, e)
-						for w := range g.Edges[e].V {
-							g2.AddVertex(w, i+1)
-							if !frontier[w] {
-								nextFrontier[w] = true
-							}
+				if _, ex := g2.Edges[e]; !ex {
+					g2.AddEdgeMapWLayer(g.Edges[e].V, e)
+					for w := range g.Edges[e].V {
+						if _, ex2 := g2.Vertices[w]; !ex2 {
+							g2.AddVertex(w, 0)
+							nextFrontier[w] = true
 						}
 					}
 				}
 			}
 		}
-		g2.SetMaxLayer(i)
 		if len(nextFrontier) == 0 {
+			for v := range frontier {
+				g2.VertexFrontier[v] = true
+			}
 			break
 		}
 		frontier = nextFrontier
+		if i == level-1 {
+			for v := range frontier {
+				g2.VertexFrontier[v] = true
+			}
+		}
 	}
 	g2.IncMap = nil
 	g2.IncMap = g.IncMap
+
+	for v := range g2.VertexFrontier {
+		cond := true
+		for e := range g2.IncMap[v] {
+			if _ ,ex := g2.Edges[e]; !ex {
+				cond = false
+				break
+			}
+		}
+		if cond {
+			delete(g2.VertexFrontier, v)
+		}
+	}
 
 	return g2
 }
 
 func ExpandFrontier(gf *HyperGraph, g *HyperGraph, level int) {
 	frontier := make(map[int32]bool)
-	oldMax := gf.MaxLayer
 
-	for vId, v := range gf.Vertices {
-		if v.Data == gf.MaxLayer+1 {
-			frontier[vId] = true
+	for v := range gf.VertexFrontier {
+		frontier[v] = true
+	}
+
+	for i := 0; i < level; i++ {
+		nextFrontier := make(map[int32]bool)
+		for v := range frontier {
+			for e := range gf.IncMap[v] {
+				if _, ex := gf.Edges[e]; !ex {
+					gf.AddEdgeMapWLayer(g.Edges[e].V, e)
+					for w := range g.Edges[e].V {
+						if _, ex2 := gf.Vertices[w]; !ex2 {
+							gf.AddVertex(w, 0)
+							nextFrontier[w] = true
+						}
+					}
+				}
+			}
+		}
+		if len(nextFrontier) == 0 {
+			gf.ClearVertexFront()
+			for v := range frontier {
+				gf.VertexFrontier[v] = true
+			}
+			break
+		}
+		frontier = nextFrontier
+		if i == level-1 {
+			gf.ClearVertexFront()
+			for v := range frontier {
+				gf.VertexFrontier[v] = true
+			}
+		}
+	}
+}
+
+func F3_ExpandFrontier(gf *HyperGraph, g *HyperGraph, remId int32, level int) {
+	frontier := make(map[int32]bool)
+	remEdge := g.Edges[remId]
+
+	for v := range remEdge.V {
+		for e := range g.IncMap[v] {
+			for w := range g.Edges[e].V {
+				if !remEdge.V[w] {
+					frontier[w] = true
+				}
+			}
+		}
+	}
+
+	// remove the edges adjacent to remEdge
+	for v := range remEdge.V {
+		for e := range gf.IncMap[v] {
+			gf.F_RemoveEdge(e, g)
 		}
 	}
 
@@ -81,67 +138,11 @@ func ExpandFrontier(gf *HyperGraph, g *HyperGraph, level int) {
 		for v := range frontier {
 			for e := range gf.IncMap[v] {
 				if _, ex := gf.Edges[e]; !ex {
-					gf.AddEdgeMapWLayer(g.Edges[e].V, oldMax+i+1, e)
+					gf.AddEdgeMapWLayer(g.Edges[e].V, e)
 					for w := range g.Edges[e].V {
 						if _, ex2 := gf.Vertices[w]; !ex2 {
-							gf.AddVertex(w, oldMax+i+2)
+							gf.AddVertex(w, 0)
 							nextFrontier[w] = true
-						}
-					}
-				}
-			}
-		}
-		if len(nextFrontier) == 0 {
-			break
-		}
-		frontier = nextFrontier
-		gf.SetMaxLayer(oldMax + i + 1)
-	}
-}
-
-func F3_ExpandFrontier(gf *HyperGraph, g *HyperGraph, remId int32, level int){
-	frontier := make(map[int32]bool)
-	remEdge := g.Edges[remId]
-	hashes := make(map[string]bool)
-
-	for v := range remEdge.V {
-		for e := range g.IncMap[v] {
-			for w := range g.Edges[e].V {
-				if !remEdge.V[w] {
-					frontier[w] = true
-				}
-			}
-		}
-	}
-
-	// remove the edges adjacent to remEdge
-	for v := range remEdge.V {
-		for e := range g.IncMap[v] {
-			g.RemoveEdge(e)
-		}
-	}
-
-	for i := 0; i < level; i++ {
-		nextFrontier := make(map[int32]bool)
-		for v := range frontier {
-			for e := range gf.IncMap[v] {
-				found := true
-				for w := range g.Edges[e].V {
-					if remEdge.V[w] {
-						found = false
-						break
-					}
-				}
-				if found {
-					hash := g.Edges[e].getHash()
-					if !hashes[hash] {
-						hashes[hash] = true
-						gf.AddEdgeMapWLayer(g.Edges[e].V, i, e)
-						for w := range g.Edges[e].V {
-							gf.AddVertex(w, i+1)
-							if !frontier[w] {
-								nextFrontier[w] = true
-							}
 						}
 					}
 				}
@@ -149,10 +150,16 @@ func F3_ExpandFrontier(gf *HyperGraph, g *HyperGraph, remId int32, level int){
 		}
 		//g2.SetMaxLayer(i)
 		if len(nextFrontier) == 0 {
+			for v := range frontier {
+				gf.VertexFrontier[v] = true
+			}
 			break
 		}
 		frontier = nextFrontier
+		if i == level -1{
+			for v := range frontier {
+				gf.VertexFrontier[v] = true
+			}
+		}
 	}
-
-
 }
