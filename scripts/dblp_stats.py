2d5784b28dd9753c3eab8bafc6d84831d7431585
KhoalaS <s4khleee@uni-trier.de>
2024-03-01 03:50:54 +0100

    rename rule columns instead of removing them

--- scripts/dblp_stats.py
+++ scripts/dblp_stats.py
@@ -22,7 +22,9 @@ args = parser.parse_args()
 df = pd.read_csv(args.file, delimiter=";")
 
 for k, v in rule_names.items():
-    df.drop(columns=[k], inplace=True)
+    #    df.drop(columns=[k], inplace=True)
+    df.rename(columns={k: v}, inplace=True)
+
 
 dblp_stats = df.describe()
 dblp_stats.drop(["count", "25%", "75%"], inplace=True)
@@ -30,4 +32,4 @@ dblp_stats.rename(index={"50%": "median"}, inplace=True)
 
 dblp_stats.drop(columns=["Vertices", "Edges"], inplace=True)
 out = open("./out/dblp_stats.md", "w+")
-out.write(dblp_stats.to_latex())
+out.write(dblp_stats.to_markdown())
