1c3e2393b4ee2ef387e84e804bf7c6309dd6bf10
KhoalaS <s4khleee@uni-trier.de>
2024-03-14 01:33:10 +0100

    dont aggregate over files

--- scripts/rome.py
+++ scripts/rome.py
@@ -1,5 +1,15 @@
 import pandas as pd
-
+print("loading csv file...")
 df = pd.read_csv("./data/rome_master.csv", delimiter=";")
-fileAgg = df.agg({"File": ["mean"]})
-print(fileAgg.head())
+
+print("file loaded...")
+df.drop(columns=["File", "OVertices", "OEdges",
+        "Vertices", "Edges"], inplace=True)
+rome_stats = df.describe()
+rome_stats.drop(["count"], inplace=True)
+rome_tbl = rome_stats.to_latex()
+
+f = open("./out/rome_stats.tex", "w+")
+f.write(rome_tbl)
+f.close()
+print(rome_stats)
