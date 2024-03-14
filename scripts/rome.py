bd9320a5ea9b8b492d37b75acbb156cd7a7d3894
KhoalaS <s4khleee@uni-trier.de>
2024-03-14 02:27:29 +0100

    add argparse

--- scripts/rome.py
+++ scripts/rome.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", metavar="FILE", help="path to csv file")
+parser.add_argument("out", metavar="OUT", help="path to output tex file")
+
+args = parser.parse_args()
+
 print("loading csv file...")
-df = pd.read_csv("./data/rome_master.csv", delimiter=";")
+df = pd.read_csv(args.file, delimiter=";")
 
 print("file loaded...")
 df.drop(columns=["File", "OVertices", "OEdges",
@@ -9,7 +17,7 @@ rome_stats = df.describe()
 rome_stats.drop(["count"], inplace=True)
 rome_tbl = rome_stats.to_latex()
 
-f = open("./out/rome_stats.tex", "w+")
+f = open(args.out, "w+")
 f.write(rome_tbl)
 f.close()
 print(rome_stats)
