923db3d087f3405c78e3eca67c0c99171390d9b6
KhoalaS <s4khleee@uni-trier.de>
2024-03-15 18:18:31 +0100

    make algos strictly exhaustive

--- pkg/alg/algorithms.go
+++ pkg/alg/algorithms.go
@@ -185,32 +185,51 @@ func ApplyRules(g *hypergraph.HyperGraph, c map[int32]bool, execs map[string]int
 
 	for {
 		kTiny := hypergraph.RemoveEdgeRule(g, c, hypergraph.TINY)
-		kVertDom := hypergraph.VertexDominationRule(g, c)
-		kTiny += hypergraph.RemoveEdgeRule(g, c, hypergraph.TINY)
-		//kEdgeDom := hypergraph.EdgeDominationRule(g)
-		kEdgeDom := 0
-		kApVertDom := hypergraph.ApproxVertexDominationRule(g, c)
-		kApDoubleVertDom := hypergraph.ApproxDoubleVertexDominationRule(g, c)
-		kSmallEdgeDegTwo, kSmallEdgeDegTwo2 := hypergraph.SmallEdgeDegreeTwoRule(g, c)
-		kTri := hypergraph.SmallTriangleRule(g, c)
-		kExtTri := hypergraph.ExtendedTriangleRule(g, c)
-		//kSmall := hypergraph.RemoveEdgeRule(g, c, hypergraph.SMALL)
-		kSmall := 0
-
 		execs["kTiny"] += kTiny
-		execs["kVertDom"] += kVertDom
-		execs["kEdgeDom"] += kEdgeDom
-		execs["kTri"] += kTri
-		execs["kExtTri"] += kExtTri
-		execs["kSmall"] += kSmall
-		execs["kApVertDom"] += kApVertDom
-		execs["kApDoubleVertDom"] += kApDoubleVertDom
-		execs["kSmallEdgeDegTwo"] += kSmallEdgeDegTwo
-		execs["kSmallEdgeDegTwo2"] += kSmallEdgeDegTwo2
 
-		if kTiny+kTri+kSmall+kApVertDom+kApDoubleVertDom+kEdgeDom+kVertDom+kExtTri+kSmallEdgeDegTwo+kSmallEdgeDegTwo2 == 0 {
-			break
+		kVertDom := hypergraph.VertexDominationRule(g, c)
+		if kVertDom > 0 {
+			execs["kVertDom"] += kVertDom
+			continue
 		}
+		//kEdgeDom := hypergraph.EdgeDominationRule(g)
+		//if kEdgeDom > 0 {
+		//	execs["kEdgeDom"] += kEdgeDom
+		//	continue
+		//}
+		kApVertDom := hypergraph.ApproxVertexDominationRule(g, c)
+		if kApVertDom > 0 {
+			execs["kApVertDom"] += kApVertDom
+			continue
+		}
+		kApDoubleVertDom := hypergraph.ApproxDoubleVertexDominationRule(g, c)
+		if kApDoubleVertDom > 0 {
+			execs["kApDoubleVertDom"] += kApDoubleVertDom
+			continue
+		}
+		kSmallEdgeDegTwo, kSmallEdgeDegTwo2 := hypergraph.SmallEdgeDegreeTwoRule(g, c)
+		if kSmallEdgeDegTwo+kSmallEdgeDegTwo2 > 0 {
+			execs["kSmallEdgeDegTwo"] += kSmallEdgeDegTwo
+			execs["kSmallEdgeDegTwo2"] += kSmallEdgeDegTwo2
+			continue
+		}
+		kTri := hypergraph.SmallTriangleRule(g, c)
+		if kTri > 0 {
+			execs["kTri"] += kTri
+			continue
+		}
+		kExtTri := hypergraph.ExtendedTriangleRule(g, c)
+		if kExtTri > 0 {
+			execs["kExtTri"] += kExtTri
+			continue
+		}
+		kSmall := hypergraph.RemoveEdgeRule(g, c, hypergraph.SMALL)
+		if kSmall > 0 {
+			execs["kSmall"] += kSmall
+			continue
+		}
+
+		break
 	}
 	return execs
 }
@@ -269,32 +288,51 @@ func ThreeHS_F3ApprPolyFrontier(g *hypergraph.HyperGraph, c map[int32]bool) map[
 func ApplyRulesFrontier(gf *hypergraph.HyperGraph, g *hypergraph.HyperGraph, c map[int32]bool, execs map[string]int, expand map[int32]bool) {
 	for {
 		kTiny := hypergraph.S_RemoveEdgeRule(gf, g, c, hypergraph.TINY, expand)
-		kVertDom := hypergraph.S_VertexDominationRule(gf, g, c, expand)
-		kTiny += hypergraph.S_RemoveEdgeRule(gf, g, c, hypergraph.TINY, expand)
-		//kEdgeDom := hypergraph.S_EdgeDominationRule(gf, g, expand)
-		kEdgeDom := 0
-		kApVertDom := hypergraph.S_ApproxVertexDominationRule(gf, g, c, expand)
-		kApDoubleVertDom := hypergraph.S_ApproxDoubleVertexDominationRule2(gf, g, c, expand)
-		kSmallEdgeDegTwo, kSmallEdgeDegTwo2 := hypergraph.S_SmallEdgeDegreeTwoRule(gf, g, c, expand)
-		kTri := hypergraph.S_SmallTriangleRule(gf, g, c, expand)
-		kExtTri := hypergraph.S_ExtendedTriangleRule(gf, g, c, expand)
-		//kSmall := hypergraph.S_RemoveEdgeRule(gf, g, c, hypergraph.SMALL, expand)
-		kSmall := 0
-
 		execs["kTiny"] += kTiny
-		execs["kVertDom"] += kVertDom
-		execs["kEdgeDom"] += kEdgeDom
-		execs["kTri"] += kTri
-		execs["kExtTri"] += kExtTri
-		execs["kSmall"] += kSmall
-		execs["kApVertDom"] += kApVertDom
-		execs["kApDoubleVertDom"] += kApDoubleVertDom
-		execs["kSmallEdgeDegTwo"] += kSmallEdgeDegTwo
-		execs["kSmallEdgeDegTwo2"] += kSmallEdgeDegTwo2
 
-		if kTiny+kTri+kSmall+kEdgeDom+kVertDom+kExtTri+kApVertDom+kApDoubleVertDom+kSmallEdgeDegTwo+kSmallEdgeDegTwo2 == 0 {
-			break
+		kVertDom := hypergraph.S_VertexDominationRule(gf, g, c, expand)
+		if kVertDom > 0 {
+			execs["kVertDom"] += kVertDom
+			continue
 		}
+		//kEdgeDom := hypergraph.S_EdgeDominationRule(gf, g, expand)
+		//if kEdgeDom > 0 {
+		//	execs["kEdgeDom"] += kEdgeDom
+		//	continue
+		//}
+		kApVertDom := hypergraph.S_ApproxVertexDominationRule(gf, g, c, expand)
+		if kApVertDom > 0 {
+			execs["kApVertDom"] += kApVertDom
+			continue
+		}
+		kApDoubleVertDom := hypergraph.S_ApproxDoubleVertexDominationRule2(gf, g, c, expand)
+		if kApDoubleVertDom > 0 {
+			execs["kApDoubleVertDom"] += kApDoubleVertDom
+			continue
+		}
+		kSmallEdgeDegTwo, kSmallEdgeDegTwo2 := hypergraph.S_SmallEdgeDegreeTwoRule(gf, g, c, expand)
+		if kSmallEdgeDegTwo+kSmallEdgeDegTwo2 > 0 {
+			execs["kSmallEdgeDegTwo"] += kSmallEdgeDegTwo
+			execs["kSmallEdgeDegTwo2"] += kSmallEdgeDegTwo2
+			continue
+		}
+		kTri := hypergraph.S_SmallTriangleRule(gf, g, c, expand)
+		if kTri > 0 {
+			execs["kTri"] += kTri
+			continue
+		}
+		kExtTri := hypergraph.S_ExtendedTriangleRule(gf, g, c, expand)
+		if kExtTri > 0 {
+			execs["kExtTri"] += kExtTri
+			continue
+		}
+		kSmall := hypergraph.S_RemoveEdgeRule(gf, g, c, hypergraph.SMALL, expand)
+		if kSmall > 0 {
+			execs["kSmall"] += kSmall
+			continue
+		}
+
+		break
 	}
 }
 
