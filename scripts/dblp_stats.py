3a6b5c6b70ab3618949602e13a2a771ecacd1b2a
KhoalaS <s4khleee@uni-trier.de>
2024-03-15 13:27:20 +0100

    output rounded values

--- scripts/dblp_stats.py
+++ scripts/dblp_stats.py
@@ -9,15 +9,21 @@ args = parser.parse_args()
 
 df = pd.read_csv(args.file, delimiter=";")
 
-for k, v in rule_names.items():
-    df.drop(columns=[k], inplace=True)
-    #df.rename(columns={k: v}, inplace=True)
-
 
 dblp_stats = df.describe()
 dblp_stats.drop(["count", "25%", "75%"], inplace=True)
 dblp_stats.rename(index={"50%": "median"}, inplace=True)
 
 dblp_stats.drop(columns=["Vertices", "Edges"], inplace=True)
-out = open("./out/dblp_stats.md", "w+")
+
+dblp_stats["Ratio"] = dblp_stats["Ratio"].round(4)
+dblp_stats["HittingSet"] = dblp_stats["HittingSet"].round(2)
+dblp_stats["Opt"] = dblp_stats["Opt"].round(2)
+
+
+for k, v in rule_names.items():
+    df.drop(columns=[k], inplace=True)
+
+
+out = open("./out/dblp_stats.tex", "w+")
 out.write(dblp_stats.to_latex())
