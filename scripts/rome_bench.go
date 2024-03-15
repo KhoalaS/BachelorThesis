19c2eaf814d4224b343bdd8ad552575d57cbac1f
KhoalaS <s4khleee@uni-trier.de>
2024-03-15 02:54:28 +0100

    add history and input flags

--- scripts/rome_bench.go
+++ scripts/rome_bench.go
@@ -15,7 +15,13 @@ import (
 func main() {
 	o := flag.String("o", "./data/rome_master.csv", "path to output csv masterfile")
 	n := flag.Int("n", 1, "number of algorithm runs per graph")
+	i := flag.String("i", "", "input graphfile")
+	hs := flag.Bool("hs", false, "enable graph history")
 	flag.Parse()
+
+	if *hs {
+		hypergraph.HistoryEnabled = true
+	}
 
 	dir, _ := os.ReadDir("./graphs/rome")
 	masterfile, _ := os.Create(*o)
@@ -23,13 +29,65 @@ func main() {
 	masterfile.WriteString(strings.Join(alg.Labels, ";"))
 	masterfile.WriteString(";OVertices;OEdges;Vertices;Edges;HittingSet\n")
 
-	bufWriter := bufio.NewWriterSize(masterfile, 1048576)
+	bufWriter := bufio.NewWriterSize(masterfile, 65536)
+
+	if len(*i) > 0 {
+		g := hypergraph.ReadFromFileRome(*i)
+		h := hypergraph.P3Detection(g)
+		eOSize := len(g.Edges)
+		vOSize := len(g.Vertices)
+		eSize := len(h.Edges)
+		vSize := len(h.Vertices)
+
+		c := make(map[int32]bool)
+
+		g.CurrentRule = "Tiny"
+		hypergraph.RemoveEdgeRule(h, c, hypergraph.TINY)
+		
+		g.CurrentRule = "VDom"
+		hypergraph.VertexDominationRule(g, c)
+		
+		g.CurrentRule = "Tiny"
+		hypergraph.RemoveEdgeRule(h, c, hypergraph.TINY)
+		
+		g.CurrentRule = "Edom"
+		hypergraph.EdgeDominationRule(g)
+		
+		g.CurrentRule = "AVD"
+		hypergraph.ApproxVertexDominationRule(g, c)
+		
+		g.CurrentRule = "ADVD"
+		hypergraph.ApproxDoubleVertexDominationRule(g, c)
+		
+		g.CurrentRule = "SED2"
+		hypergraph.SmallEdgeDegreeTwoRule(g, c)
+		
+		g.CurrentRule = "Tri"
+		hypergraph.SmallTriangleRule(g, c)
+		
+		g.CurrentRule = "ETri"
+		hypergraph.ExtendedTriangleRule(g, c)
+		
+		g.CurrentRule = "Small"
+		hypergraph.RemoveEdgeRule(g, c, hypergraph.SMALL)
+
+		execs := alg.ThreeHS_F3ApprPoly(h, c, 0)
+
+		rules := ""
+		for _, label := range alg.Labels {
+			rules += strconv.Itoa(execs[label])
+			rules += ";"
+		}
+		bufWriter.WriteString((fmt.Sprintf("%s;%f;%s%d;%d;%d;%d;%d\n", *i, alg.GetRatio(execs), rules, vOSize, eOSize, vSize, eSize, len(c))))
+		bufWriter.Flush()
+		return
+	}
 
 	for _, file := range dir {
 		if file.Name() == "Graph.log" {
 			continue
 		}
-	
+
 		for i := 0; i < *n; i++ {
 			g := hypergraph.ReadFromFileRome("./graphs/rome/" + file.Name())
 			h := hypergraph.P3Detection(g)
