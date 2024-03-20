69b65e4baf94ca89b29827123d80233b6cbe2903
KhoalaS <s4khleee@uni-trier.de>
2024-03-20 21:10:32 +0100

    edit script

--- scripts/dblp_stats.py
+++ scripts/dblp_stats.py
@@ -4,11 +4,15 @@ from opts import rule_names
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", metavar="FILE", help="path to csv file")
+parser.add_argument("out", metavar="OUT", help="path to output file")
 
 args = parser.parse_args()
 
 df = pd.read_csv(args.file, delimiter=";")
 
+
+for k, v in rule_names.items():
+    df.drop(columns=[k], inplace=True)
 
 dblp_stats = df.describe()
 dblp_stats.drop(["count", "25%", "75%"], inplace=True)
@@ -19,11 +23,7 @@ dblp_stats.drop(columns=["Vertices", "Edges"], inplace=True)
 dblp_stats["Ratio"] = dblp_stats["Ratio"].round(4)
 dblp_stats["HittingSet"] = dblp_stats["HittingSet"].round(2)
 dblp_stats["Opt"] = dblp_stats["Opt"].round(2)
+dblp_stats["Time"] = dblp_stats["Time"].round()
 
-
-for k, v in rule_names.items():
-    df.drop(columns=[k], inplace=True)
-
-
-out = open("./out/dblp_stats.tex", "w+")
-out.write(dblp_stats.to_latex())
+out = open(args.out, "w+")
+out.write(dblp_stats.to_latex(float_format="{:.4f}".format))
