d63b254b4eb0464b322e2611dda1282640989fd6
KhoalaS <s4khleee@uni-trier.de>
2024-03-21 20:19:43 +0100

    add table formatting to format.py

--- format.py
+++ format.py
@@ -10,11 +10,13 @@ for line in f:
 r_0 = r"(\$\$)\s(\\begin\{align\*\}\s[^$]*)(\$\$)"
 r_1 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}\[H\].*?end{algorithm})\s(\$\$)", re.DOTALL)
 r_2 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}.*?end{algorithm})\s(\$\$)", re.DOTALL)
+r_3 = re.compile(r"(\$\$)\s(\\begin\{table\}\[t\].*?end{table})\s(\$\$)", re.DOTALL)
 
 out = open("./out/out.MD", "w+")
 text = re.sub(r_0, r"\2", text)
 text = re.sub(r_1, r"\2", text)
 text = re.sub(r_2, r"\2", text)
+text = re.sub(r_3, r"\2", text)
 
 out.write(text)
 out.close()
