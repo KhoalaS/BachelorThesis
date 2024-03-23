baf7ba4f2afed72b83d84900e362aa2e289395ed
KhoalaS <s4khleee@uni-trier.de>
2024-03-23 20:57:38 +0100

    add regex for figures

--- format.py
+++ format.py
@@ -11,12 +11,14 @@ r_0 = r"(\$\$)\s(\\begin\{align\*\}\s[^$]*)(\$\$)"
 r_1 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}\[H\].*?end{algorithm})\s(\$\$)", re.DOTALL)
 r_2 = re.compile(r"(\$\$)\s(\\begin\{algorithm\}.*?end{algorithm})\s(\$\$)", re.DOTALL)
 r_3 = re.compile(r"(\$\$)\s(\\begin\{table\}\[t\].*?end{table})\s(\$\$)", re.DOTALL)
+r_4 = re.compile(r"(\$\$)\s(\\begin\{figure\}\[t\].*?end{figure})\s(\$\$)", re.DOTALL)
 
 out = open("./out/out.MD", "w+")
 text = re.sub(r_0, r"\2", text)
 text = re.sub(r_1, r"\2", text)
 text = re.sub(r_2, r"\2", text)
 text = re.sub(r_3, r"\2", text)
+text = re.sub(r_4, r"\2", text)
 
 out.write(text)
 out.close()
