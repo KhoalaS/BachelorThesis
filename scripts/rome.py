bdd26174c0effd5667a74d88d6236d5e1b35f2dc
KhoalaS <s4khleee@uni-trier.de>
2024-03-25 22:45:19 +0100

    update rome scripts

--- scripts/rome.py
+++ scripts/rome.py
@@ -21,8 +21,9 @@ rome_stats = df.describe()
 rome_stats.drop(["count", "25%", "75%"], inplace=True)
 rome_stats.rename(index={"50%": "median"}, inplace=True)
 rome_stats["Ratio"] = rome_stats["Ratio"].round(4)
-rome_stats["HittingSet"] = rome_stats["HittingSet"].round(2)
-rome_stats["Opt"] = rome_stats["Opt"].round(2)
+rome_stats["HittingSet"] = rome_stats["HittingSet"].round()
+rome_stats["Opt"] = rome_stats["Opt"].round()
+rome_stats.rename(columns={"Ratio": "ratio", "HittingSet": "$|C|$", "Opt": "est. opt"}, inplace=True)
 
 for k, v in rule_names.items():
     rome_stats[k] = rome_stats[k].round(2)
--- scripts/rome_bench.go
+++ scripts/rome_bench.go
@@ -15,7 +15,6 @@ import (
 func main() {
 	o := flag.String("o", "./data/rome_master.csv", "path to output csv masterfile")
 	n := flag.Int("n", 1, "number of algorithm runs per graph")
-	i := flag.String("i", "", "input graphfile")
 	flag.Parse()
 
 	dir, _ := os.ReadDir("./graphs/rome")
@@ -25,43 +24,6 @@ func main() {
 	masterfile.WriteString(";OVertices;OEdges;Vertices;Edges;HittingSet;Opt\n")
 
 	bufWriter := bufio.NewWriterSize(masterfile, 65536)
-
-	if len(*i) > 0 {
-		g := hypergraph.ReadFromFileRome(*i)
-		h := hypergraph.P3Detection(g)
-		eOSize := len(g.Edges)
-		vOSize := len(g.Vertices)
-		eSize := len(h.Edges)
-		vSize := len(h.Vertices)
-
-		c := make(map[int32]bool)
-		execs := alg.MakeExecs()
-
-		for len(h.Edges) > 0 {
-			execs["kTiny"] += hypergraph.RemoveEdgeRule(h, c, hypergraph.TINY)
-			execs["kVertDom"] += hypergraph.VertexDominationRule(h, c)
-			execs["kTiny"] += hypergraph.RemoveEdgeRule(h, c, hypergraph.TINY)
-			hypergraph.WriteToFileSimple(h, "./out/rome_single_pre_edom.txt")
-			execs["kEdgeDom"] += hypergraph.EdgeDominationRule(h)
-			execs["kApVertDom"] += hypergraph.ApproxVertexDominationRule(h, c)
-			execs["kApDoubleVertDom"] += hypergraph.ApproxDoubleVertexDominationRule(h, c)
-			k0, k1 := hypergraph.SmallEdgeDegreeTwoRule(h, c)
-			execs["kSmallEdgeDegTwo"] += k0
-			execs["kSmallEdgeDegTwo2"] += k1
-			execs["kTri"] += hypergraph.SmallTriangleRule(h, c)
-			execs["kExtTri"] += hypergraph.ExtendedTriangleRule(h, c)
-			execs["kSmall"] += hypergraph.RemoveEdgeRule(h, c, hypergraph.SMALL)
-		}
-
-		rules := ""
-		for _, label := range alg.Labels {
-			rules += strconv.Itoa(execs[label])
-			rules += ";"
-		}
-		bufWriter.WriteString((fmt.Sprintf("%s;%f;%s%d;%d;%d;%d;%d;%d\n", *i, alg.GetRatio(execs), rules, vOSize, eOSize, vSize, eSize, len(c), alg.GetEstOpt(execs))))
-		bufWriter.Flush()
-		return
-	}
 
 	nFiles := len(dir)
 
