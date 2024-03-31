36cbdf455268db8cbc9c86b0a5fbd7657483bc2d
KhoalaS <s4khleee@uni-trier.de>
2024-03-31 23:22:01 +0200

    edit script

--- scripts/dblp_stats.py
+++ scripts/dblp_stats.py
@@ -12,7 +12,8 @@ df = pd.read_csv(args.file, delimiter=";")
 
 
 for k, v in rule_names.items():
-    df.drop(columns=[k], inplace=True)
+#    df.drop(columns=[k], inplace=True)
+    df.rename(columns={k: v}, inplace=True)
 
 dblp_stats = df.describe()
 dblp_stats.drop(["count", "25%", "75%"], inplace=True)
@@ -23,7 +24,7 @@ dblp_stats.drop(columns=["Vertices", "Edges"], inplace=True)
 dblp_stats["Ratio"] = dblp_stats["Ratio"].round(4)
 dblp_stats["HittingSet"] = dblp_stats["HittingSet"].round(2)
 dblp_stats["Opt"] = dblp_stats["Opt"].round(2)
-dblp_stats["Time"] = dblp_stats["Time"].round()
+#dblp_stats["Time"] = dblp_stats["Time"].round()
 
 out = open(args.out, "w+")
 out.write(dblp_stats.to_latex(float_format="{:.4f}".format))
