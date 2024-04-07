9eddac82789f33df4f167007d16c7b40a05dab93
Khoa Le <s4khleee@uni-trier.de>
2024-04-07 20:51:59 +0000

    rename nom to num

--- pkg/alg/utils.go
+++ pkg/alg/utils.go
@@ -14,14 +14,14 @@ func MakeExecs() map[string]int{
 }
 
 func GetRatio(execs map[string]int) float64 {
-	var nom float64 = 0
+	var num float64 = 0
 	var denom float64 = 0
 
 	for key, val := range execs {
-		nom += float64(Ratios[key].A * val)
+		num += float64(Ratios[key].A * val)
 		denom += float64(Ratios[key].B * val)
 	}
-	return nom / denom
+	return num / denom
 }
 
 func GetEstOpt(execs map[string]int) int {
